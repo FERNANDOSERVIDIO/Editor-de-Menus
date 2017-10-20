f987321092b7b2d37a368a99b158cd1549aa9687
MalloLucas <32558973+MalloLucas@users.noreply.github.com>
2017-10-19 22:28:58 -0300

    Add files via upload

---
+++ ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ChangeScene : MonoBehaviour {
+
+    // Use this for initialization
+    public Button yourButton;
+
+    void Start()
+    {
+        yourButton = this.GetComponent<Button>();
+        yourButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+}
---
+++ MenuBuilder.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+
+public class MenuBuilder : EditorWindow
+{
+    GameObject NewButton;
+    GameObject NewTextButton;
+    Canvas ActualCanvas;
+    Sprite PrefabSprite;
+    bool CustomSize;
+    public MonoScript functionButton;
+    
+   
+
+    [MenuItem("Editor Windows/MenuBuilder")]
+    static void CreateWindow()
+    {
+        ((MenuBuilder)GetWindow(typeof(MenuBuilder))).Show();
+    }
+
+    void OnGUI()
+    {
+        PrefabSprite = (Sprite)EditorGUILayout.ObjectField("Prefab Button Image", PrefabSprite, typeof(Sprite), true);
+        ActualCanvas = FindObjectOfType<Canvas>();
+
+        functionButton = (MonoScript)EditorGUILayout.ObjectField("Function to Add: ", functionButton , typeof(MonoScript), true);
+        
+
+        if (GUILayout.Button("Build Button"))
+        {
+
+            
+            NewButton = new GameObject("Button");
+            NewButton.AddComponent<RectTransform>();
+            NewButton.AddComponent<CanvasRenderer>();
+            NewButton.AddComponent<Image>();
+            NewButton.AddComponent<Button>();
+          
+            NewButton.GetComponent<Image>().preserveAspect = true;
+            NewButton.GetComponent<RectTransform>().position = new Vector3(0, 0, 0);
+
+            
+
+            NewTextButton = new GameObject("Text");
+            NewTextButton.AddComponent<RectTransform>();
+            NewTextButton.AddComponent<CanvasRenderer>();
+            NewTextButton.AddComponent<Text>();
+            NewTextButton.GetComponent<Text>().text = "New Text";
+            NewTextButton.GetComponent<Text>().color = Color.black;
+
+
+
+            NewButton.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 30);
+            NewTextButton.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 30);
+            NewTextButton.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+
+            NewTextButton.transform.parent = NewButton.transform;
+            NewButton.transform.parent = ActualCanvas.transform;
+
+            NewButton.GetComponent<Image>().sprite = PrefabSprite;
+
+        }
+    }
+
+   
+}
